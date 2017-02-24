b4605898cddad574f95f5d74401820c3c7928c55
designerror <designerror@yandex.ru>
2017-02-24 19:01:37 +0300

    Update default.cmake

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -24,7 +24,7 @@ include(hunter_user_error)
 
 # NOTE: no names with spaces!
 
-hunter_config(Buffer VERSION 0.1.2)
+hunter_config(Buffer VERSION 0.1.3)
 hunter_config(AllTheFlopsThreads VERSION 0.1-p0)
 hunter_config(Android-Apk VERSION 1.1.13)
 hunter_config(Android-Build-Tools VERSION 22.0.1)
