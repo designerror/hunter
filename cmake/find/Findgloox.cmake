93fec4b3afef40d2e0184c40493ece9c641a8b2e
designerror <designerror@yandex.ru>
2017-03-23 20:52:29 +0300

    Update Findgloox.cmake

--- cmake/find/Findgloox.cmake
+++ cmake/find/Findgloox.cmake
@@ -14,13 +14,14 @@ set(GLOOX_INCLUDE_DIRS "${GLOOX_INCLUDE_DIR}")
 if(GLOOX_LIBRARIES AND GLOOX_INCLUDE_DIR)
 	message(STATUS "Found gloox: ${GLOOX_LIBRARIES}")
 	set(GLOOX_FOUND 1)
-  if (NOT TARGET "gloox::gloox")
-    add_library("gloox::gloox" UNKNOWN IMPORTED)
-    set_target_properties("gloox::gloox"
-      PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${GLOOX_INCLUDE_DIRS}"
-      IMPORTED_LOCATION "${GLOOX_LIBRARY}"
-    )
+  	if(NOT TARGET "gloox::gloox")
+  		add_library("gloox::gloox" UNKNOWN IMPORTED)
+    		set_target_properties("gloox::gloox"
+      			PROPERTIES
+      			INTERFACE_INCLUDE_DIRECTORIES "${GLOOX_INCLUDE_DIRS}"
+      			IMPORTED_LOCATION "${GLOOX_LIBRARY}"
+    		)
+	endif()
 else(GLOOX_LIBRARIES AND GLOOX_INCLUDE_DIR)
 	message( STATUS "Could NOT find gloox" )
 endif( GLOOX_LIBRARIES AND GLOOX_INCLUDE_DIR )
